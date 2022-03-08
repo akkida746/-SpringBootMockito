116579f1ebdc53b0b8a4a316763ffd2b68b684c5
Akash <akkida746@gmail.com>
2022-03-08 12:41:20 +0530

    refactor

--- src/main/java/com/example/mocking/MockingApplication.java
+++ src/main/java/com/example/mocking/MockingApplication.java
@@ -3,6 +3,7 @@ package com.example.mocking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//https://www.baeldung.com/spring-mock-rest-template
 @SpringBootApplication
 public class MockingApplication {
 
--- src/test/java/com/example/mocking/MockingApplicationTests.java
+++ src/test/java/com/example/mocking/MockingApplicationTests.java
@@ -17,14 +17,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.ExpectedCount;
+import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
+
+//https://www.baeldung.com/spring-mock-rest-template
 @SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
 class MockingApplicationTests {
@@ -33,6 +45,12 @@ class MockingApplicationTests {
 	String testurl;
 	@Mock
 	RestTemplate restTemplate = new RestTemplate();
+	MockRestServiceServer mockRestServiceServer;
+
+	@Before
+	public void init(){
+		mockRestServiceServer = MockRestServiceServer.createServer(restTemplate);
+	}
 
 	@Test
 	void serviceTest() throws IOException {
